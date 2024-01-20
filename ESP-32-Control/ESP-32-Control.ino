c426ec1d39e0921a4e847c163fa4e203b56e7151
Kevin Palma <123513375+KevinPalma21@users.noreply.github.com>
2024-01-19 23:16:20 -0600

    Update ESP-32-Control.ino

--- ESP-32-Control/ESP-32-Control.ino
+++ ESP-32-Control/ESP-32-Control.ino
@@ -1,9 +1,7 @@
+#include <PS4Controller.h>
+#include <BluetoothSerial.h>
+
 void setup() {
-  // put your setup code here, to run once:
 
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-
+  Serial.begin(115200) //This is the default baud rate this allows for faster data transfer
 }
