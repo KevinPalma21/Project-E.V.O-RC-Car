70e39318fe5ddcf6abaaea89013260e072930230
Kevin Palma <123513375+KevinPalma21@users.noreply.github.com>
2024-01-20 15:40:53 -0600

    Update ESP-32-Control.ino

--- ESP-32-Control/ESP-32-Control.ino
+++ ESP-32-Control/ESP-32-Control.ino
@@ -5,7 +5,8 @@ void setup() {
 
   Serial.begin(115200); //This is the default baud rate this allows for faster data transfer
 
-  if(!PS4.begin("Mac Address of ESP 32")) // This if statement will only execute if True so when it fails to connect it will return True and run the code
+  if(!PS4.begin("Mac Address of ESP 32")){ // This if statement will only execute if True so when it fails to connect it will return True and run the code
    Serial.println("Ps4 Controller not paired"); // This is will allow the microcontroller and the computer to communicate (serial) and print the following response
    while(1); // This will make it go into a infinite loop which will make the program stop or in other words get stuck here if the controller doesn't connnect
 }
+ Serial.println("Ps4 controller Connected")
