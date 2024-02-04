a9bce2a3fb13e4ce5c86ff92a6bf8ae472a4e573
Kevin Palma <123513375+KevinPalma21@users.noreply.github.com>
2024-02-04 16:12:58 -0600

    Update Button_Instrcutions.c

--- ESP-32-Control/Button_Instrcutions.c
+++ ESP-32-Control/Button_Instrcutions.c
@@ -1,23 +1,23 @@
 void loop(){
 
-if (PS4.isConnected()) {
-  
-  if(PS4.Button(PS4_BUTTON_CROSS){
+if (PS4.isConnected()) {           // This will call the function if it is true then it will run the code
+
+  if(PS4.Button(PS4_BUTTON_CROSS){         // If Cross button is pressed this will run
    CrossButtonPress();
    Serial.println("Cross Button");
   }
   
-  if(PS4.Button(PS4_BUTTON_CIRCLE){
-   CircleButtonPress();
+  if(PS4.Button(PS4_BUTTON_CIRCLE){        // If Circle button is pressed this will run
+   CircleButtonPress(); 
    Serial.prinln("Circle Button");
   }
 
-   if(PS4.Button(PS4_BUTTON_TRIANGLE){
+   if(PS4.Button(PS4_BUTTON_TRIANGLE){     // If Triangle button is pressed this will run
    TriangleButtonPress();
    Serial.prinln("Triangle Button");
    }
 
-   if(PS4.Button(PS4_BUTTON_SQUARE){
+   if(PS4.Button(PS4_BUTTON_SQUARE){       // If Square button is pressed this will run
    SqaureButtonPress();
    Serial.prinln("Square Button");
    }
