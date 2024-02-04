15a50360a4fd02db93102dc764c6f53c9402bd84
Kevin Palma <123513375+KevinPalma21@users.noreply.github.com>
2024-02-04 16:11:11 -0600

    Update Button_Instrcutions.c

--- ESP-32-Control/Button_Instrcutions.c
+++ ESP-32-Control/Button_Instrcutions.c
@@ -1,10 +1,26 @@
 void loop(){
 
 if (PS4.isConnected()) {
+  
   if(PS4.Button(PS4_BUTTON_CROSS){
    CrossButtonPress();
+   Serial.println("Cross Button");
+  }
+  
+  if(PS4.Button(PS4_BUTTON_CIRCLE){
+   CircleButtonPress();
+   Serial.prinln("Circle Button");
   }
 
+   if(PS4.Button(PS4_BUTTON_TRIANGLE){
+   TriangleButtonPress();
+   Serial.prinln("Triangle Button");
+   }
+
+   if(PS4.Button(PS4_BUTTON_SQUARE){
+   SqaureButtonPress();
+   Serial.prinln("Square Button");
+   }
 
 
 
