1b98b9ec2ca21f739a81acc18b66f44fba0c48eb
Kevin Palma <123513375+KevinPalma21@users.noreply.github.com>
2024-02-07 02:56:07 -0600

    Update Button_Instrcutions.c

--- ESP-32-Control/Button_Instrcutions.c
+++ ESP-32-Control/Button_Instrcutions.c
@@ -7,7 +7,10 @@ void loop(){
  if (PS4.isConnected()) {
   int R2Value = PS4.R2Value()  // This will return a value depedning how hard the R2 value is being pressed.
   if (PS4.R2 > 0){
-    int pWmValue = map(R2Value, 0, 255, 0, 255) // This Translates the pressure given to a value we will use for control 
+    int pWmValue = map(R2Value, 0, 255, 0, 255); // This Translates the pressure given to a value we will use for control 
+    ledcWrtie(channel, pWmValue); // This is the amount of power based on the pressure out that was read from previous line
+    
+
 
     
     
