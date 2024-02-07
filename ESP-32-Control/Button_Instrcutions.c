e490052b6d9de46caaac1c9223b334626a9a404d
Kevin Palma <123513375+KevinPalma21@users.noreply.github.com>
2024-02-07 02:31:00 -0600

    Update Button_Instrcutions.c

--- ESP-32-Control/Button_Instrcutions.c
+++ ESP-32-Control/Button_Instrcutions.c
@@ -1,28 +1,15 @@
+#include <PS4Controller.h>
+
+
+
+
 void loop(){
+ if (PS4.isconnected()) {
+  if (PS4.R1()){
+    ledcWrite(channel,128)
 
-if (PS4.isConnected()) {           // This will call the function if it is true then it will run the code
 
-  if(PS4.Button(PS4_BUTTON_CROSS){         // If Cross button is pressed this will run
-   CrossButtonPress();
-   Serial.println("Cross Button");
-  }
-  
-  if(PS4.Button(PS4_BUTTON_CIRCLE){        // If Circle button is pressed this will run
-   CircleButtonPress(); 
-   Serial.prinln("Circle Button");
+
   }
 
-   if(PS4.Button(PS4_BUTTON_TRIANGLE){     // If Triangle button is pressed this will run
-   TriangleButtonPress();
-   Serial.prinln("Triangle Button");
-   }
-
-   if(PS4.Button(PS4_BUTTON_SQUARE){       // If Square button is pressed this will run
-   SqaureButtonPress();
-   Serial.prinln("Square Button");
-   }
-  
-  if(PS4.Button())
-
-
-}
+ }
