1ff818c1e729a1fcaff1e393a365966ae970647a
Kevin Palma <123513375+KevinPalma21@users.noreply.github.com>
2024-02-07 02:59:54 -0600

    Update Button_Instrcutions.c

--- ESP-32-Control/Button_Instrcutions.c
+++ ESP-32-Control/Button_Instrcutions.c
@@ -9,7 +9,10 @@ void loop(){
   if (PS4.R2 > 0){
     int pWmValue = map(R2Value, 0, 255, 0, 255); // This Translates the pressure given to a value we will use for control 
     ledcWrtie(channel, pWmValue); // This is the amount of power based on the pressure out that was read from previous line
-    
+  ]
+  else {
+    ledcWrite(channel, 0);       // If no pressure is detected the power will be by default set to zero
+  }
 
 
     
